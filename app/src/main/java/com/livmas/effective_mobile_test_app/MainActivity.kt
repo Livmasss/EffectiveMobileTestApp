3f0fec149e32970a4e9de4bb5b5d7016f21517bb
livmas <seiranyan.sg@gmail.com>
2024-02-13 18:47:38 +0300

    Initial commit

---
+++ app/src/main/java/com/livmas/effective_mobile_test_app/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.livmas.effective_mobile_test_app
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.livmas.effective_mobile_test_app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.navView.setupWithNavController(
+            findNavController(R.id.nav_host_fragment_activity_main)
+        )
+    }
+}
