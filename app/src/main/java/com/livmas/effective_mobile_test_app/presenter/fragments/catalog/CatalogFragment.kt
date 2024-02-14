55a94230b180bb080c93fc17d55ed59b6cc69161
livmas <seiranyan.sg@gmail.com>
2024-02-14 16:13:53 +0300

    Commit 1.6
    For CatalogFragment create sorting (spinner) and filter.

--- app/src/main/java/com/livmas/effective_mobile_test_app/presenter/fragments/catalog/CatalogFragment.kt
+++ app/src/main/java/com/livmas/effective_mobile_test_app/presenter/fragments/catalog/CatalogFragment.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.fragment.app.viewModels
 import com.livmas.effective_mobile_test_app.R
 import com.livmas.effective_mobile_test_app.databinding.FragmentCatalogBinding
 import com.livmas.effective_mobile_test_app.presenter.fragments.BaseNavigationFragment
+
 
 class CatalogFragment : BaseNavigationFragment() {
 
@@ -25,5 +28,16 @@ class CatalogFragment : BaseNavigationFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         mainActivity.setTitleResource(R.string.title_catalog_page)
+
+        val spinner: Spinner = binding.spinner
+
+        val adapter: ArrayAdapter<String> = ArrayAdapter<String>(
+            requireContext(),
+            R.layout.icon_sorting_spinner_layout, R.id.tvSorting,
+            resources.getStringArray(R.array.sorting_types)
+        )
+        adapter.setDropDownViewResource(R.layout.sorting_spinner_layout)
+
+        spinner.adapter = adapter
     }
 }
